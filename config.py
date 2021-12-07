b3caede7562545a49e4c9e80de4a234be5ede360
b20lopez <92145309+pluniverse@users.noreply.github.com>
2021-12-07 08:12:58 +0800

    up
    up

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BASE_KIT_VERSION = "V1.3.1"
 PREFIX = "/"
-TOKEN = ""
+TOKEN = "OTE3NTM3MjY2NjkwMjk3ODU2.Ya6JKw.n3jL84wycpquOd_D7SqwSYxZ7b8"
 ERROR_WEBHOOK_URL = "" #Optional, but very useful
 EMBED_COLOR = 0xff69b4 #Pink, you can put in any hex color here, just replace the # with 0x (Example: #FFFFF would need to be entered as 0xFFFFF)
